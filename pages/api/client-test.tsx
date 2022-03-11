bc527b16804940dfa32bbfedefe27498fb1e1846
jw.kim <orochi13@naver.com>
2022-03-11 16:29:37 +0900

    feat: :sparkles: server side

--- pages/api/client-test.tsx
+++ pages/api/client-test.tsx
@@ -1,18 +1,11 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import client from "../../libs/client";
+import { PrismaClient } from "@prisma/client";
 
-export default async function handler(
-    req: NextApiRequest, res:NextApiResponse
-){
-
-    await client.user.create({
-        data:{
-            email: 'test@test.com',
-            name: 'hi'
-        }
-    })
-
-    res.json({
-        ok: true,
-    })
+declare global {
+  var client: PrismaClient | undefined;
 }
+
+const client = global.client || new PrismaClient();
+
+if (process.env.NODE_ENV === "development") global.client = client;
+
+export default client;
