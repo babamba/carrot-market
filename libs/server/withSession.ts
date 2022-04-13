0164334a4daa16a756c18e2709c14ae343eecf3a
jw <orochi13@naver.com>
2022-04-13 09:19:37 +0900

    add session cookie 15 minute ttl

--- libs/server/withSession.ts
+++ libs/server/withSession.ts
@@ -1,22 +1,25 @@
-import { withIronSessionApiRoute, withIronSessionSsr } from "iron-session/next";
+import { withIronSessionApiRoute, withIronSessionSsr } from 'iron-session/next';
+import { IronSessionOptions } from 'iron-session';
 
-declare module "iron-session" {
+declare module 'iron-session' {
     interface IronSessionData {
         user?: {
             id: number;
         };
     }
 }
+const thirdtyMinutesInSeconds = 60 * 30;
 
-const cookieOptions = {
-    cookieName: "carrotsession",
+const IRON_OPTIONS: IronSessionOptions = {
+    cookieName: 'carrotsession',
     password: process.env.COOKIE_PASSWORD!,
+    ttl: thirdtyMinutesInSeconds,
 };
 
 export function withApiSession(fn: any) {
-    return withIronSessionApiRoute(fn, cookieOptions);
+    return withIronSessionApiRoute(fn, IRON_OPTIONS);
 }
 
 export function withSsrSession(handler: any) {
-    return withIronSessionSsr(handler, cookieOptions);
+    return withIronSessionSsr(handler, IRON_OPTIONS);
 }
